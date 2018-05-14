78b6a396c19f28e2851a29d766446a84fc384161
amolinero <amolinero@Casiopea>
2018-05-15 00:19:36 +0200

    Package repositories rename

--- plugyourcar-backend/src/main/java/com/plugyourcar/backend/repositories/PasswordRepository.java
+++ plugyourcar-backend/src/main/java/com/plugyourcar/backend/repositories/PasswordRepository.java
@@ -1,4 +1,4 @@
-package com.plugyourcar.repositories;
+package com.plugyourcar.backend.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
